b72ef3471a42126864bb3e8976d50d6531bf12e8
Pra-kriti <cer1dumru@gmail.com>
2019-05-09 13:37:35 +0545

    drag and drop working for list and partly for cards

--- src/components/Board/List/index.js
+++ src/components/Board/List/index.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 import SimpleStorage from 'react-simple-storage';
 import { Input, Card, Form, Icon, Button } from 'antd';
 
-import uuid from 'uuid';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
-// let id = 0;
+import uuid from 'uuid';
 
 class List extends Component {
     constructor(props) {
@@ -172,86 +172,223 @@ class List extends Component {
         }) 
     }
 
+    onDragEnd = result => {
+        const { destination, source, draggableId, type } = result;
+        if(!destination){
+            return;
+        }
+
+        if(
+            destination.droppableId === source.droppableId &&
+            destination.index === source.index
+        ) {
+            return;
+        }
+
+        if (type === 'column'){
+            const newListOrder = Array.from(this.state.listOrder);
+            newListOrder.splice(source.index, 1);
+            newListOrder.splice(destination.index, 0, draggableId);
+            const newState = {
+                ...this.state,
+                listOrder: newListOrder
+            };
+            this.setState(newState);
+            return;
+        }
+
+        const home = this.state.list[source.droppableId];
+        const foreign = this.state.list[destination.droppableId];
+
+        // Reorder if moving card within list
+        if(home === foreign){
+            const newCardId = Array.from(home.taskId);
+            newCardId.splice(source.index, 1);
+            newCardId.splice(destination.index, 0, draggableId);
+            const newList = {
+                ...home,
+                taskId: newCardId
+            }
+            const newState = {
+                ...this.state,
+                list: {
+                    ...this.state.list,
+                    [newList.id]: newList
+                }
+            };
+            this.setState(newState);
+            return;
+        }
+
+        // Moving between list
+        const homeTaskId = Array.from(home.taskId);
+        homeTaskId.splice(source.index, 1);
+        const newHome = {
+            ...home,
+            taskId: homeTaskId
+        }
+        const foreignTaskId = Array.from(foreign.taskId);
+        foreignTaskId.splice(destination.index, 0, draggableId);
+        const newForeign = {
+            ...foreign,
+            taskId: foreignTaskId
+        }
+        const newState = {
+            ...this.state,
+            list: {
+                ...this.state.list,
+                [newHome.id]: newHome,
+                [newForeign.id]: newForeign
+            }
+        };
+        this.setState(newState);
+    }
+
     render() {
-        const {inputListTitle, listOrder } = this.state;
+        const {inputListTitle, listOrder, list } = this.state;
         return(
             <div className='board-body'>
-                {/* <SimpleStorage parent={this} prefix='test' /> */}
-                <div className='lists-flex-row'>
-                    {listOrder.map((listId, index) => {
-                        const list = this.state.list[listId];
-                        return (
-                            <div className='lists-flex-row'>
-                                {list.title
-                                ?
-                                    <div className='add-list'>
-                                        <div className='list-body'>
-                                            {/* <span className='list-heading'>{list.title}</span> */}
-                                            <Input id='list-heading' value={list.title} onChange={(e) => this.handleInputTitleChange(listId, e)} />
-                                            <Icon className='icon-right' type='cross' style={{color: 'red'}} onClick={(e) => this.handleDeleteList(listId, e)} />
-                                        </div>
-
-                                        {list.taskId.map(id => (
-                                            this.state.card[id].body
-                                            ?
-                                            <div className='list-card'>
-                                                <p className='card-title'>{this.state.card[id].body}</p>
-                                            </div>  
-                                            :
-                                            null  
-                                        ))}                                
+                <DragDropContext onDragEnd={this.onDragEnd}>
+                    <Droppable
+                        droppableId='all-columns'
+                        direction='horizontal'
+                        type='column'
+                        key={list.id}
+                    >
+                        {(provided) => (
+                            <div
+                                {...provided.droppableProps}
+                                ref={provided.innerRef}                                
+                            >
+                                {/* <SimpleStorage parent={this} prefix='test' /> */}                                
+                                <div className='lists-flex-row'>
+                                {listOrder.map((listId, index) => {                                    
+                                    const list = this.state.list[listId];
+                                    return (
                                         
-                                        {this.state.list[listId].inputCardTitle 
-                                        ?
-                                        <div className='add-card'>
-                                            <Form className='list-form'>
-                                                <Icon id='cross-icon' type='cross' style={{color: 'red'}} onClick={(e) => this.handleDeleteCard(listId, this.state.list[listId].taskId[this.state.list[listId].taskId.length-1], e)} />
-                                                <Input name='list-title' id='list-title-input' size='large' placeholder='Enter title of the card' onChange={(e) => this.handleCardTitleChange(this.state.list[listId].taskId[this.state.list[listId].taskId.length-1], e)} />                        
-                                                <Button id='list-title-submit' type='primary' onClick={(e) => this.handleCardTitleSubmit(listId, e)} >Submit</Button>    
-                                            </Form>
-                                        </div>
-                                        :
-                                        <Button 
-                                            size='large'
-                                            onClick={(e) => this.handleAddCardClick(e, listId)}
-                                            className='add-card-btn'
-                                        >
-                                            <Icon type='plus' />Add a card
-                                        </Button>
-                                        }
-                                    </div>                                
-                                :
-                                null
-                                }
-                            </div>
-                        )
-                    })}
+                                        <Draggable
+                                            draggableId={listId}
+                                            index={index}
+                                        >                                            
+                                            {(provided)=> (
+                                                <div
+                                                    {...provided.draggableProps}
+                                                    ref={provided.innerRef}                                                    
+                                                    {...provided.dragHandleProps}
+                                                >
+                                                    <div className='lists-flex-row'>
+                                                        {list.title
+                                                        ?
+                                                            <div className='add-list'>
+                                                                <div className='list-body'>
+                                                                    {/* <span className='list-heading'>{list.title}</span> */}
+                                                                    <Input id='list-heading' value={list.title} onChange={(e) => this.handleInputTitleChange(listId, e)} />
+                                                                    <Icon className='icon-right' type='cross' style={{color: 'red'}} onClick={(e) => this.handleDeleteList(listId, e)} />
+                                                                </div>
+                                                                {provided.placeholder}
 
-                    <div className='lists-flex-column'>
-                        <Button 
-                            size='large'
-                            onClick={this.handleClick}
-                            className='add-list-btn'
-                        >
-                            <Icon type='plus' />
-                            Add another list
-                        </Button>
+                                                                <Droppable
+                                                                    droppableId={listId}
+                                                                    type='task'
+                                                                >
+                                                                    {(provided) => (
+                                                                        <div
+                                                                            ref={provided.innerRef}
+                                                                            {...provided.droppableProps}
+                                                                        >
+                                                                            {provided.placeholder}
+                                                                            {list.taskId.map((id, index) => (
+                                                                                <Draggable
+                                                                                    key={id}
+                                                                                    draggableId={id}
+                                                                                    index={index}
+                                                                                >
+                                                                                    {(provided) => (
+                                                                                        <div                                                                                            
+                                                                                            {...provided.draggableProps}
+                                                                                            {...provided.dragHandleProps}
+                                                                                            ref={provided.innerRef}
+                                                                                        >
+                                                                                            {provided.placeholder}
+                                                                                            {this.state.card[id].body
+                                                                                            ? 
+                                                                                            <div className='list-card'>
+                                                                                                <p className='card-title'>{this.state.card[id].body}</p>
+                                                                                            </div>  
+                                                                                            : 
+                                                                                            null}                                                          
+                                                                                        </div>
+                                                                                    )
+                                                                                }                                                                                
+                                                                                </Draggable>                                                                                
+                                                                            ))}
 
-                        {inputListTitle 
-                        ?
-                        <div className='add-list'>
-                            <Form className='list-form' onSubmit={this.handleSubmit}>
-                                <Icon id='cross-icon' type='cross' style={{color: 'red'}} onClick={this.handleDelete} />
-                                <Input name='list-title' id='list-title-input' size='large' placeholder='Enter title of the list' onChange={(e) => this.handleInputTitleChange(listOrder[listOrder.length-1], e)} />                        
-                                <Button id='list-title-submit' type='primary' onClick={this.handleTitleSubmit} >Submit</Button>    
-                            </Form>
+                                                                        </div>
+                                                                    )}
+                                                                    
+                                                                </Droppable>                                                                                                
+                                                                
+                                                                {this.state.list[listId].inputCardTitle 
+                                                                ?
+                                                                <div className='add-card'>
+                                                                    <Form className='list-form'>
+                                                                        <Icon id='cross-icon' type='cross' style={{color: 'red'}} onClick={(e) => this.handleDeleteCard(listId, this.state.list[listId].taskId[this.state.list[listId].taskId.length-1], e)} />
+                                                                        <Input name='list-title' id='list-title-input' size='large' placeholder='Enter title of the card' onChange={(e) => this.handleCardTitleChange(this.state.list[listId].taskId[this.state.list[listId].taskId.length-1], e)} />                        
+                                                                        <Button id='list-title-submit' type='primary' onClick={(e) => this.handleCardTitleSubmit(listId, e)} >Submit</Button>    
+                                                                    </Form>
+                                                                </div>
+                                                                :
+                                                                <Button 
+                                                                    size='large'
+                                                                    onClick={(e) => this.handleAddCardClick(e, listId)}
+                                                                    className='add-card-btn'
+                                                                >
+                                                                    <Icon type='plus' />Add a card
+                                                                </Button>
+                                                                }
+                                                            </div>                                
+                                                        :
+                                                        null
+                                                        }
+                                                    </div>
+                                                </div>
+                                            )}
+                                                                                                                                
+                                        </Draggable>
+                                    )
+                                })}         
+                                {provided.placeholder}                       
+
+                                <div className='lists-flex-column'>
+                                    <Button 
+                                        size='large'
+                                        onClick={this.handleClick}
+                                        className='add-list-btn'
+                                    >
+                                        <Icon type='plus' />
+                                        Add another list
+                                    </Button>
+
+                                    {inputListTitle 
+                                    ?
+                                    <div className='add-list'>
+                                        <Form className='list-form' onSubmit={this.handleSubmit}>
+                                            <Icon id='cross-icon' type='cross' style={{color: 'red'}} onClick={this.handleDelete} />
+                                            <Input name='list-title' id='list-title-input' size='large' placeholder='Enter title of the list' onChange={(e) => this.handleInputTitleChange(listOrder[listOrder.length-1], e)} />                        
+                                            <Button id='list-title-submit' type='primary' onClick={this.handleTitleSubmit} >Submit</Button>    
+                                        </Form>
+                                    </div>
+                                    :
+                                    null
+                                    }
+                                </div>  
+
+                            </div>                            
                         </div>
-                        :
-                        null
-                        }
-                    </div>
-                </div>
-                
+                        )}
+                        
+                    </Droppable>
+                </DragDropContext>  
             </div>
         );
     }
